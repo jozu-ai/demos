4158fe4d2b4bbd31cf5fa1b0a7c4fa44d7720036
Brad Micklea <7644938+bmicklea@users.noreply.github.com>
2025-12-02 13:40:53 -0500

    added fraud example
    Signed-off-by: Brad Micklea <7644938+bmicklea@users.noreply.github.com>

---
+++ cli-demos/pack-and-push/fraud.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+###################################################
+# Kit demo showing packing and pushing a ModelKit #
+# Recorded with Asciinema                         #
+###################################################
+
+# Demo Magic configuration
+source ../demo-magic.sh
+
+# speed at which to simulate typing. bigger num = faster
+TYPE_SPEED=70
+
+# custom prompt
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+DEMO_PROMPT="${GREEN}➜ ${CYAN}\W ${COLOR_RESET}"
+
+# text color
+# DEMO_CMD_COLOR=$BLACK
+
+# Disable line wrapping (makes it easier to read kit list output)
+printf '\e[?7l' 
+
+# Use the current date to version our modelkit (so we don't overwrite others)
+CURRENT_DATE=$(date +"%F")
+
+# Make sure the local registry doesn't have our modelkit in it
+kit remove jozu.ml/jozu-quickstarts/fraud-detection:$CURRENT_DATE
+kit remove --all
+
+# hide configuration
+clear
+
+cd fraud-modelkit
+pe "tree"
+
+printf "\n"
+pe "more ./Kitfile"
+
+printf "\n"
+pe "kit pack . -t fraud-detection:$CURRENT_DATE"
+
+printf "\n"
+pe "kit list"
+
+printf "\n"
+pe "kit push fraud-detection:$CURRENT_DATE jozu.ml/jozu-quickstarts/fraud-detection:$CURRENT_DATE"
+
+cd ..
+
+# wait max 3 seconds until user presses
+# PROMPT_TIMEOUT=3
+# wait
+
+# enters interactive mode and allows newly typed command to be executed
+# cmd
+
+# show a prompt so as not to reveal our true nature after
+# the demo has concluded
+printf "\n"
+p ""
